f03e478def31a4823ad1d326034d07a82cc813c3
narasingammarimuthu <Narasingam@zasya.online>
2024-01-30 21:03:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("hello world")
+
+name = "Narasingam";
+
+message = "How are you";
+
+print("Hi" + " " + name + " " + message)
+print(name.upper())
+print(name.lower() + "\n" +name.upper())
+name = "seenu"
+print(name.title())
+len=len(name)
+
+num = 10
+
+print(len+num)
+print(len)
+
+
+print(name.replace("seenu","ammu"))
+
+print(name.isalnum())
