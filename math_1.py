2a8074fbfd52d4e5d9426aacdfab1c1c0fea3815
narasingammarimuthu <Narasingam@zasya.online>
2024-01-30 21:49:13 +0530

    practice input and math functions

---
+++ math_1.py
@@ -0,0 +1,52 @@
+import math
+
+
+a = 10
+b = 5
+
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+print(a + 100)
+print(a - (100*3))
+
+
+a = 9.51
+print(round(a))
+a = 9.49
+print(round(a))
+
+a = -10
+
+print(abs(a))
+
+a=5
+
+print(pow(a,5))
+
+a = 4.08
+
+print(math.ceil(a))
+
+print(math.floor(a))
+
+print(math.factorial(9))
+
+a = 10
+
+print(a/3)
+
+
+print("log2")
+
+print(math.log2(a))
+
+print("cos")
+
+print(math.cos(a))
+
+ 
---
+++ user_input.py
@@ -0,0 +1,24 @@
+name = input("Please enter your name : ")
+
+print("Hello"+ " " + name)
+
+height = float(input( "Hi"  + name + "Please enter your height : "))
+
+height_inc = "{:.2f}".format(height/2.54)
+
+print( "Hi" + " " + name + " " + "Your height is " + " " + str(height) + " CM")
+
+print(" and \n your height is " +  height_inc  + "Inches")
+
+
+# excersise assignment
+
+
+name = input("Please enter your name ")
+email = input("please enter your email")
+phone = input("please enter your mobile number")
+
+
+print("Hi" + " " + name)
+print("your mail id is " + " " + email)
+print("Your mobile number is" + " " + phone) 
