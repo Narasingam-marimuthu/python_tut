bb0642ef4f8ef16d7df863515135c1ab19a8368b
narasingammarimuthu <Narasingam@zasya.online>
2024-01-30 21:17:35 +0530

    multi assign and type casting practice

---
+++ multiple_assign.py
@@ -0,0 +1,35 @@
+
+name,age,year = "Narasingam",24,2000
+
+print(name )
+
+myAge = myFrdAge = 24
+
+print(myAge + myFrdAge)
+
+
+name = 'Narasingam'
+age = 24
+year = 2000
+
+# type casting
+
+print ("your age is " + str(age) + " " +"and your frd age is" + " " + str(myFrdAge) )
+
+print(type(age))
+
+age = "24"
+
+print( "Next year your age is" + " " + str( int(age) + 1) )
+
+
+# excersise assignment
+
+
+name = "Vasanth"
+
+days = 15
+
+year = 2024
+
+print("Dear" + " " + name + " " + "you have" + " " +str(days) + " days of leave balance for this year(" + str(year) + ")" )
