c3e6015808bbb5c77bf193384c2d88c81bdf4d6b
narasingammarimuthu <Narasingam@zasya.online>
2024-01-31 21:00:04 +0530

    is else statement practice

---
+++ ifelse.py
@@ -0,0 +1,24 @@
+
+num = int(input("Enter ther numeric value : "))
+
+if num%2 == 0 :
+    print(" Entered number is a even number")
+elif num == 11:
+    print("Entered No is 11 ")    
+else :
+    print(" Entered number is a odd number")    
+    
+
+letter = input("Enter the Alpha : ")
+
+print(type(letter))
+
+if letter == 'A' or letter =='a' :
+    print("A for Apple")
+elif letter == "B" or letter =="b":
+    print("B for Ball")
+elif letter == "C" or letter =="c":
+    print("C for cat")
+else :
+    print("Unknown alpha")    
+    
